952770386986de9888cc224ad7df541b14254011
Richardson Gunde <152559661+richard-devbot@users.noreply.github.com>
2024-11-24 23:23:45 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//         port: '',
+//         pathname: "/u/**"
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//         port: '',
+//         pathname: "/a/**"
+//       }
+//     ]
+//   },
+//   experimental: {
+//     serverActions: {
+//       allowedOrigins: ["localhost:3000"]
+//     }
+//   }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -18,8 +45,16 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"]
+      allowedOrigins: ["http://localhost:3000", "https://waigenie.vercel.app"]
     }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*' // Adjust this for production
+      }
+    ];
   }
 };
 
